e85a536c7695777ac2849ca83c41444885f6afc0
vinaybadgujar102 <vinaybadgujar8@gmail.com>
2023-09-04 21:01:35 +0530

    add: password encryption

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,30 +1,53 @@
 import jwt from 'jsonwebtoken';
-import User from '../model/User.js'; // Import your User model here
+import bcrypt from 'bcrypt';
+import User from '../model/User.js'; 
 
-const SECRET = 'your-secret-key'; // Replace with your actual secret key
+const SECRET = 'your-secret-key'; 
 
 export const signup = async (req, res) => {
   const { username, password } = req.body;
-  const user = await User.findOne({ username });
 
-  if (user) {
-    res.status(403).json({ message: 'User already exists' });
-  } else {
-    const newUser = new User({ username, password });
+  try {
+    const existingUser = await User.findOne({ username });
+
+    if (existingUser) {
+      return res.status(403).json({ message: 'User already exists' });
+    }
+
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    const newUser = new User({ username, password: hashedPassword });
     await newUser.save();
+
     const token = jwt.sign({ username, role: 'user' }, SECRET, { expiresIn: '1h' });
     res.json({ message: 'User created successfully', token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
   }
 };
 
 export const login = async (req, res) => {
   const { username, password } = req.headers;
-  const user = await User.findOne({ username, password });
 
-  if (user) {
+  try {
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(403).json({ message: 'Invalid username or password' });
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+      return res.status(403).json({ message: 'Invalid username or password' });
+    }
+
     const token = jwt.sign({ username, role: 'user' }, SECRET, { expiresIn: '1h' });
     res.json({ message: 'Logged in successfully', token });
-  } else {
-    res.status(403).json({ message: 'Invalid username or password' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
   }
 };
